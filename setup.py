90c6705e97192ac3875121aeb54a7e9ecf1cd54e
fujino takuro <phy.l.050492@gmail.com>
2015-10-28 20:37:51 +0900

    set up file and so on

---
+++ setup.py
@@ -0,0 +1,30 @@
+
+import os
+import argparse
+import commands
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--prefix',help='install directory',default='$HOME/opt')
+args = parser.parse_args()
+
+if not os.path.exists(args.prefix):
+    print "install path does'nt exit"
+    exit()
+
+fpath = os.path.dirname(os.path.abspath(__file__))
+binpath = os.path.join(args.prefix,'bin')
+#print fpath
+
+pydir = os.path.join(fpath,'pycode')
+print '@ '+pydir
+pyfiles = commands.getoutput('ls -l %s/*.py' % pydir).split('\n')
+
+for p in pyfiles:
+    if p[3] == 'x':
+        p = p.split(' ')
+        fname = os.path.basename(p[-1])
+        if os.path.exists(os.path.join(binpath,fname)):
+            print fname+'is already exist, cannot link!'
+            continue
+        #print p[-1],binpath
+        os.system('ln -s %s %s' % (p[-1],binpath))
