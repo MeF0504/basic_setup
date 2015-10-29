65ed2f30858315846942ca3a7a90b75cb0b7a325
fujino takuro <phy.l.050492@gmail.com>
2015-10-29 22:26:01 +0900

    iterm setting

--- setup/ssh-host-color.sh
+++ setup/ssh-host-color.sh
@@ -34,7 +34,7 @@ elif [[ "$@" =~ kekcc ]]; then
 elif [[ "$@" =~ nersc ]]; then
   set_term_bgcolor 0 20 15
 elif [[ "$@" =~ gt ]]; then
-  set_term_bgcolor 20 10 0
+  set_term_bgcolor 13 5 10
 fi
 
 ssh $@
