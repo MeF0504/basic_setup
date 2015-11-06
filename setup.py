ce17f93f4bf40c35e2048d5321a6e0eebf296390
fujino takuro <phy.l.050492@gmail.com>
2015-11-06 17:19:03 +0900

    adjust

--- setup.py
+++ setup.py
@@ -8,18 +8,21 @@ parser.add_argument('--prefix',help='install directory',default='$HOME/opt')
 args = parser.parse_args()
 
 if not os.path.exists(args.prefix):
-    print "install path does'nt exit"
+    print "install path %s does not exit" % args.prefix
     exit()
 
 fpath = os.path.dirname(os.path.abspath(__file__))
 binpath = os.path.join(args.prefix,'bin')
 #print fpath
+os.chdir(fpath)
 
 pydir = os.path.join(fpath,'pycode')
 print '\n@ '+pydir+'\n'
-pyfiles = commands.getoutput('ls -l %s/*' % pydir).split('\n')
+#pyfiles = commands.getoutput('ls -l %s/*' % pydir).split('\n')
+pyfiles = commands.getoutput('zsh -c "ls %s/*(.x)"' % pydir).split('\n')
 
 for p in pyfiles:
+    """
     if p[3] == 'x':
         p = p.split(' ')
         fname = os.path.basename(p[-1])
@@ -29,3 +32,11 @@ for p in pyfiles:
         #print p[-1],binpath
         os.system('ln -s %s %s' % (p[-1],binpath))
         print 'linked '+fname
+    """
+    #print p
+    fname = os.path.basename(p)
+    if os.path.exists(os.path.join(binpath,fname)):
+        print fname+' is already exist, cannot link!'
+        continue
+    os.system('ln -s %s %s' % (p,binpath))
+    print 'linked '+fname
