d49810ee566b2f7a90a95c692bd14f8447bc2e42
fujino takuro <phy.l.050492@gmail.com>
2015-11-14 15:33:33 +0900

    add vim

--- setup.py
+++ setup.py
@@ -3,8 +3,16 @@ import os
 import argparse
 import commands
 
+def mkdir(path):
+    path = os.path.expanduser(path)
+    if not os.path.exists(path):
+        print 'mkdir '+path
+        os.mkdir(path)
+        os.chmod(path,0755)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--prefix',help='install directory',default=os.path.expanduser('~/opt'))
+parser.add_argument('--download',help='download some files (from git)',action='store_true')
 args = parser.parse_args()
 
 if not os.path.exists(args.prefix):
@@ -13,11 +21,11 @@ if not os.path.exists(args.prefix):
 
 fpath = os.path.dirname(os.path.abspath(__file__))
 binpath = os.path.join(args.prefix,'bin')
-if not os.path.exists(binpath):
-    os.mkdir(binpath)
+mkdir(binpath)
 #print fpath
 os.chdir(fpath)
 
+############### python script directory ###############
 pydir = os.path.join(fpath,'pycode')
 print '\n@ '+pydir+'\n'
 #pyfiles = commands.getoutput('ls -l %s/*' % pydir).split('\n')
@@ -32,30 +40,74 @@ for p in pyfiles:
     os.system('ln -s %s %s' % (p,binpath))
     print 'linked '+fname
 
+############### basic setup directory ###############
 setdir = os.path.join(fpath,'setup')
 print '\n@ '+setdir+'\n'
-setfiles = ('zshrc_file','256colors.pl','ssh-host-color.sh','terminator_config')
-for fy in setfiles:
-    fpath = os.path.join(setdir,fy)
-    if os.path.exists(fpath):
-        if 'zshrc_file' in fpath:
-            print 'copy %s --> ~/.zshrc' % os.path.basename(fpath)
-            os.system('cp -i %s ~/.zshrc' % fpath)
+files = ('zshrc_file','256colors.pl','ssh-host-color.sh','terminator_config')
+for fy in files:
+    spath = os.path.join(setdir,fy)
+    if os.path.exists(spath):
+        if 'zshrc_file' in spath:
+            print 'copy %s --> ~/.zshrc' % os.path.basename(spath)
+            os.system('cp -i %s ~/.zshrc' % spath)
             if not os.path.exists(os.path.expanduser('~/.zshrc.mine')):
                 with open(os.path.expanduser('~/.zshrc.mine'),'a') as f:
                     print >> f,'## PC dependent zshrc'
                     print >> f,'#'
                     print >> f,'\n'
 
-        if '256colors.pl' in fpath:
-            print 'copy %s --> %s' % (os.path.basename(fpath),binpath)
-            os.system('cp -i %s %s' % (fpath,binpath))
+        if '256colors.pl' in spath:
+            print 'copy %s --> %s' % (os.path.basename(spath),binpath)
+            os.system('cp -i %s %s' % (spath,binpath))
 
-        if ('ssh-host-color.sh' in fpath) and (os.uname()[0]=='Darwin'):
-            print 'copy %s --> %s' % (os.path.basename(fpath),binpath)
-            os.system('cp -i %s %s' % (fpath,binpath))
+        if ('ssh-host-color.sh' in spath) and (os.uname()[0]=='Darwin'):
+            print 'copy %s --> %s' % (os.path.basename(spath),binpath)
+            os.system('cp -i %s %s' % (spath,binpath))
 
-        if ('terminator_config' in fpath) and (os.uname()[0]=='Linux') and os.path.exists(os.path.expanduser('~/.config/terminator')):
-            print 'copy %s --> ~/.config/terminator/config' % os.path.basename(fpath)
-            os.system('cp -i %s ~/.config/terminator/config' % fpath)
+        if ('terminator_config' in spath) and (os.uname()[0]=='Linux') and os.path.exists(os.path.expanduser('~/.config/terminator')):
+            print 'copy %s --> ~/.config/terminator/config' % os.path.basename(spath)
+            os.system('cp -i %s ~/.config/terminator/config' % spath)
+
+############### vim setup directory ###############
+vimdir = os.path.join(fpath,'vim')
+print '\n@ '+vimdir+'\n'
+
+files = ('vimrc_file','vimrc_color','vimrc_plugin','vimrc_neobundle','vimrc_neocomp')
+mkdir('~/.vim')
+mkdir('~/.vim/rcdir')
+
+if args.download:
+    mkdir('tmp')
+    os.chdir(os.path.join(fpath,'tmp'))
+    print 'clone neobundle'
+    mkdir('~/.vim/bundle/')
+    os.system('git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim')
+    print '\nclone inkpot'
+    os.system('git clone https://github.com/ciaranm/inkpot')
+    os.system('cp -ri ./inkpot/colors ~/.vim')
+    print '\nclone seiya'
+    os.system('git clone https://github.com/miyakogi/seiya.vim')
+    os.system('cp -ri ./seiya.vim/plugin ~/.vim')
+    print '\nremove download tmp files'
+    os.system('rm -rf %s' % os.path.join(fpath,'tmp','*'))
+    os.chdir(fpath)
+
+for fy in files:
+    vpath = os.path.join(vimdir,fy)
+    if os.path.exists(vpath):
+        if 'vimrc_file' in vpath:
+            print 'copy %s --> ~/.vimrc' % os.path.basename(vpath)
+            os.system('cp -i %s ~/.vimrc' % vpath)
+        if 'vimrc_color' in vpath:
+            print 'copy %s --> ~/.vim/rcdir' % os.path.basename(vpath)
+            os.system('cp -i %s ~/.vim/rcdir/.vimrc.color' % vpath)
+        if 'vimrc_plugin' in vpath:
+            print 'copy %s --> ~/.vim/rcdir' % os.path.basename(vpath)
+            os.system('cp -i %s ~/.vim/rcdir/.vimrc.plugin' % vpath)
+        if 'vimrc_neobundle' in vpath:
+            print 'copy %s --> ~/.vim/rcdir' % os.path.basename(vpath)
+            os.system('cp -i %s ~/.vim/rcdir/.vimrc.neobundle' % vpath)
+        if 'vimrc_neocomp' in vpath:
+            print 'copy %s --> ~/.vim/rcdir' % os.path.basename(vpath)
+            os.system('cp -i %s ~/.vim/rcdir/.vimrc.neocomplcache' % vpath)
 
