cc4f20ea466c10c5f2da571a9abe22541138333a
fujino takuro <phy.l.050492@gmail.com>
2015-12-28 00:47:37 +0900

    adjust

--- pycode/fnum.py
+++ pycode/fnum.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import os
 import subprocess
 if float(sys.version[:3]) < 2.7:
     import commands
@@ -9,13 +10,17 @@ if len(sys.argv) < 2:
     wd = '.'
 else:
     wd = sys.argv[1]
+"""
 try:
     files = int(subprocess.check_output('ls -l %s | wc -l' % wd,shell=True))
 except AttributeError:
     files = int(commands.getoutput('ls -l %s | wc -l' % wd))
+"""
+files = len(os.listdir(wd))
 
 if len(sys.argv) > 2:
     files = len(sys.argv)
+    files -= 1
 
-print files-1
+print files
 
