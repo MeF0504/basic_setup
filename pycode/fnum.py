4cb8454d59e07a6d8d9afbffea9663880b7aa384
fujino takuro <phy.l.050492@gmail.com>
2015-12-28 01:04:15 +0900

    improve

--- pycode/fnum.py
+++ pycode/fnum.py
@@ -2,21 +2,25 @@
 
 import sys
 import os
+import glob
 import subprocess
-if float(sys.version[:3]) < 2.7:
-    import commands
+#if float(sys.version[:3]) < 2.7:
+    #import commands
+
+def len1(wd):
+    wd = os.path.join(wd,'*')
+    return len(glob.glob(wd))
 
 if len(sys.argv) < 2:
     wd = '.'
+    files = len1(wd)
 else:
     wd = sys.argv[1]
-"""
-try:
-    files = int(subprocess.check_output('ls -l %s | wc -l' % wd,shell=True))
-except AttributeError:
-    files = int(commands.getoutput('ls -l %s | wc -l' % wd))
-"""
-files = len(os.listdir(wd))
+#try:
+    #files = int(subprocess.check_output('ls -l %s | wc -l' % wd,shell=True))
+#except AttributeError:
+    #files = int(commands.getoutput('ls -l %s | wc -l' % wd))
+    files = len1(wd)
 
 if len(sys.argv) > 2:
     files = len(sys.argv)
