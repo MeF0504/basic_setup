a08192bed993e74f4239609fc406f01f2af09429
fujino takuro <phy.l.050492@gmail.com>
2016-02-08 13:00:35 +0900

    improve

--- pycode/fnum.py
+++ pycode/fnum.py
@@ -3,28 +3,34 @@
 import sys
 import os
 import glob
-import subprocess
-#if float(sys.version[:3]) < 2.7:
-    #import commands
+import argparse
 
 def len1(wd):
     wd = os.path.join(wd,'*')
     return len(glob.glob(wd))
 
-if len(sys.argv) < 2:
-    wd = '.'
-    files = len1(wd)
-else:
-    wd = sys.argv[1]
-#try:
-    #files = int(subprocess.check_output('ls -l %s | wc -l' % wd,shell=True))
-#except AttributeError:
-    #files = int(commands.getoutput('ls -l %s | wc -l' % wd))
-    files = len1(wd)
+def len2(wd):
+    wd = os.path.join(wd,'.*')
+    return len(glob.glob(wd))
 
-if len(sys.argv) > 2:
-    files = len(sys.argv)
-    files -= 1
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files',help='target files and directories',nargs='*')
+    parser.add_argument('-a','--all',help='Include directory entries whose names begin with a dot',action='store_true')
+    args = parser.parse_args()
 
-print files
+    if len(args.files) == 0:
+        wd = '.'
+        lenfile = len1(wd)
+        if args.all:
+            lenfile += len2(wd)
+    elif len(args.files)==1 and os.path.isdir(args.files[0]):
+        wd = args.files[0]
+        lenfile = len1(wd)
+        if args.all:
+            lenfile += len2(wd)
+    else:
+        lenfile = len(args.files)
+
+    print lenfile
 
