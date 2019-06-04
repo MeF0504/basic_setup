9d45f0ba3df655d0761bdbbcf57d9d280154df85
fujino_air <fujino.takuro.92@gmail.com>
2019-06-04 19:09:20 +0900

    enable to display cpu and memory information (beta version).

--- codes/version.py
+++ codes/version.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 
 import os
+import sys
 import subprocess
 
-def linux():
+def linux_version():
     release_files = ['/etc/redhat-release','/etc/lsb-release','/etc/issue']
     for rf in release_files:
         if os.path.exists(rf):
@@ -11,16 +12,66 @@ def linux():
             subprocess.call('cat '+rf,shell=True)
             exit()
 
-def mac():
+def linux_cpu():
+    subprocess.call('cat /proc/cpuinfo', shell=True)
+    
+def linux_mem():
+    subprocess.call('cat /proc/meminfo', shell=True)
+
+
+def mac_version():
     #print subprocess.check_call('sw_vers',shell=True)
     subprocess.call('sw_vers',shell=True)
-    exit()
 
-uname = os.uname()[0]
-if uname == 'Darwin':
-    mac()
-elif uname == 'Linux':
-    linux()
+def mac_cpu():
+    subprocess.call('system_profiler SPHardwareDataType', shell=True)
 
-else:
-    print "can't find version file. please add version file place in version.py!"
+def mac_mem():
+    subprocess.call('system_profiler SPHardwareDataType', shell=True)
+
+
+if __name__ == "__main__":
+    uname = os.uname()[0]
+    if (len(sys.argv) <= 1) or (sys.argv[1] == "version"):
+        if uname == 'Darwin':
+            mac_version()
+        elif uname == 'Linux':
+            linux_version()
+        else:
+            print("can't find version file. please add version file place in version.py!")
+
+    elif sys.argv[1] == "cpu":
+        if uname == 'Darwin':
+            mac_cpu()
+        elif uname == 'Linux':
+            linux_cpu()
+        else:
+            print("can't find cpu file. please add version file place in version.py!")
+
+    elif sys.argv[1] == "memory":
+        if uname == 'Darwin':
+            mac_mem()
+        elif uname == 'Linux':
+            linux_mem()
+            print("can't find memory file. please add version file place in version.py!")
+
+    elif sys.argv[1] == "all":
+        if uname == 'Darwin':
+            mac_version()
+            mac_cpu()
+        elif uname == 'Linux':
+            linux_version()
+            linux_cpu()
+            linux_mem()
+
+    else:
+        print("""
+usage: version.py [command]
+
+enable commands are following;
+version : display version.
+cpu     : display cpu information.
+memory  : display memory information.
+all     : display version, cpu, and memory information.
+""")
+
