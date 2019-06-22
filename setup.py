592f1856a596647371b11ddd8202259133612dfd
Takuro Fujino <fujino.takuro.92@gmail.com>
2019-06-22 23:08:46 +0900

    nvim response 1
    change to make ~/.config/nvim directory and copy (or symbolic link) here.
    also, change directory constitution.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 import os
-#import sys
+import os.path as op
 import argparse
 import subprocess
 import glob
@@ -9,11 +9,11 @@ import glob
 
 
 def mkdir(path):
-    path = os.path.expanduser(path)
-    if not os.path.exists(path):
+    path = op.expanduser(path)
+    if not op.exists(path):
         print 'mkdir '+path
-        os.mkdir(path)
-        os.chmod(path,0755)
+        os.makedirs(path, mode=0o755)
+        #os.chmod(path,0755)
 
 def fcopy(file1,file2,link=False,force=False,**kwargs):
     def fcopy_main(cmd,comment,test):
@@ -23,8 +23,8 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
         else:
             print 'cmd check:: '+cmd+'\n'
 
-    name1 = os.path.basename(file1)
-    name2 = os.path.basename(file2)
+    name1 = op.basename(file1)
+    name2 = op.basename(file2)
 
     if kwargs.has_key('test'):
         test = kwargs['test']
@@ -40,7 +40,7 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
         cmd = 'ln -s %s %s' % (file1, file2)
         comment = 'linked '+name1
 
-        if (not os.path.exists(file2)) and condition:
+        if (not op.exists(file2)) and condition:
             fcopy_main(cmd,comment,test)
         elif condition:
             print '[  %s  ] is already exist, cannot link!' % name2
@@ -51,7 +51,7 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
 
         if force and condition:
             fcopy_main(cmd, comment, test)
-        elif (not os.path.exists(file2)) and condition:
+        elif (not op.exists(file2)) and condition:
             fcopy_main(cmd, comment, test)
         elif condition:
             yn = raw_input('[  %s  ] is already exist, are you realy overwite? [y,n]' % name2)
@@ -62,9 +62,8 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prefix',help='install directory',default=os.path.expanduser('~/opt'))
+    parser.add_argument('--prefix',help='install directory',default=op.expanduser('~/opt'))
     parser.add_argument('--download',help='download some files (from git)',action='store_true')
-    #parser.add_argument('--copy',help="only copy files instead of link",action='store_true')
     parser.add_argument('--link',help="link files instead of copy",action='store_true')
     parser.add_argument('--test',help="don't copy, just show command",action='store_true')
     parser.add_argument('-f','--force',help="Do not prompt for confirmation before overwriting the destination path",action='store_true')
@@ -72,37 +71,37 @@ if __name__ == "__main__":
 
     #print dir(args)
     #exit()
-    if not os.path.exists(args.prefix):
+    if not op.exists(args.prefix):
         print "install path %s does not exit" % args.prefix
         exit()
 
-    fpath = os.path.dirname(os.path.abspath(__file__))
-    binpath = os.path.join(args.prefix,'bin')
+    fpath = op.dirname(op.abspath(__file__))
+    binpath = op.join(args.prefix,'bin')
     mkdir(binpath)
     #print fpath
     os.chdir(fpath)
 
     ############### script directory ###############
-    codedir = os.path.join(fpath,'codes')
+    codedir = op.join(fpath,'codes')
     print '\n@ '+codedir+'\n'
     codefiles = []
-    for cfy in glob.glob(os.path.join(codedir,'*')):
+    for cfy in glob.glob(op.join(codedir,'*')):
         if os.access(cfy,os.X_OK):
-            if (os.path.basename(cfy) == 'ssh-host-color.ssh'):
+            if (op.basename(cfy) == 'ssh-host-color.ssh'):
                 if (os.uname()[0] =='Darwin'):
                     codefiles.append(cfy)
-            elif (os.path.basename(cfy) == 'pdf2jpg'):
+            elif (op.basename(cfy) == 'pdf2jpg'):
                 if (os.uname()[0] == 'Darwin'):
                     codefiles.append(cfy)
             else:
                 codefiles.append(cfy)
 
     for p in codefiles:
-        fname = os.path.basename(p)
-        fcopy(p,os.path.join(binpath,fname),link=args.link,force=args.force,test=args.test)
+        fname = op.basename(p)
+        fcopy(p,op.join(binpath,fname),link=args.link,force=args.force,test=args.test)
 
     ############### basic setup directory ###############
-    setdir = os.path.join(fpath,'setup')
+    setdir = op.join(fpath,'setup')
     print '\n@ '+setdir+'\n'
     files_mac = {\
                 'zshrc_file':'~/.zshrc', \
@@ -122,59 +121,40 @@ if __name__ == "__main__":
         files = {}
 
     for fy in files:
-        spath = os.path.join(setdir,fy)
-        if os.path.exists(spath):
-            if 'zshrc_file' == fy:
-                fcopy(spath,os.path.expanduser(files[fy]),link=bool(args.link),force=args.force,test=args.test)
-                if not os.path.exists(os.path.expanduser('~/.zshrc.mine')):
-                    with open(os.path.expanduser('~/.zshrc.mine'),'a') as f:
-                        print >> f,'## PC dependent zshrc'
-                        print >> f,'#'
-                        print >> f,'\n'
+        spath = op.join(setdir,fy)
+        if op.exists(spath):
+            fcopy(spath,files[fy],force=args.force,test=args.test)
 
-            #elif 'terminator_config' == fy:
-                #fcopy(spath,os.path.expanduser(files[fy]),link=False,force=args.force,condition=(os.uname()[0]=='Linux') and os.path.exists(os.path.expanduser(files[fy])),test=args.test)
+    zshrc_mine = op.expanduser('~/.zshrc.mine')
+    if not op.exists(zshrc_mine):
+        with open(zshrc_mine,'a') as f:
+            print >> f,'## PC dependent zshrc'
+            print >> f,'#'
+            print >> f,'\n'
 
-            else:
-                fcopy(spath,files[fy],force=args.force,test=args.test)
 
     ############### vim setup directory ###############
-    vimdir = os.path.join(fpath,'vim')
+    vimdir = op.join(fpath,'vim')
     print '\n@ '+vimdir+'\n'
 
-    files = { \
-            'vimrc':'~/.vimrc', \
-            'vimrc_color':'vimrc.color', \
-            'vimrc_plugin':'vimrc.plugin', \
-            'vimrc_func':'vimrc.func', \
-            'vimrc_dein':'vimrc.dein', \
-            'python.vim':'python.vim',\
-            'vim.vim':'vim.vim',\
-            'qf.vim':'qf.vim',\
-            'help.vim':'help.vim',\
-            'tex.vim':'tex.vim',\
-            'javascript.vim':'javascript.vim',\
-            'html.vim':'html.vim',\
-            'css.vim':'css.vim',\
-            }
-    mkdir('~/.vim')
-    mkdir('~/.vim/rcdir')
-    mkdir('~/.vim/swp')
-    mkdir('~/.vim/ftplugin')
-    rcdir = os.path.expanduser('~/.vim/rcdir')
-    ftdir = os.path.expanduser('~/.vim/ftplugin')
+    vim_config_dir = op.expanduser('~/.config/nvim')
+    rcdir = op.join(vim_config_dir, 'rcdir')
+    ftdir = op.join(vim_config_dir, 'ftplugin')
+    mkdir(rcdir)
+    mkdir(ftdir)
+    mkdir(op.join(vim_config_dir, "swp"))
 
     if args.download:
         mkdir('tmp')
-        os.chdir(os.path.join(fpath,'tmp'))
+        os.chdir(op.join(fpath,'tmp'))
 
         #print '\nclone neobundle'
         #mkdir('~/.vim/bundle/')
         #subprocess.call('git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim',shell=True)
 
         print '\nclone dein'
-        mkdir('~/.vim/dein')
-        subprocess.call('curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && chmod u+x installer.sh && ./installer.sh ~/.vim/dein',shell=True)
+        mkdir('~/.config/nvim/dein')
+        subprocess.call('curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && chmod u+x installer.sh && ./installer.sh ~/.config/nvim/dein',shell=True)
 
         print '\nclone inkpot'
         subprocess.call('git clone https://github.com/ciaranm/inkpot',shell=True)
@@ -200,32 +180,38 @@ if __name__ == "__main__":
         subprocess.call('cp -ri ./current-func-info.vim/plugin ~/.vim',shell=True)
 
         print '\nremove download tmp files'
-        subprocess.call('rm -rf %s' % os.path.join(fpath,'tmp','*'),shell=True)
+        subprocess.call('rm -rf %s' % op.join(fpath,'tmp','*'),shell=True)
         os.chdir(fpath)
 
-    for fy in files:
-        vpath = os.path.join(vimdir,fy)
-        if os.path.exists(vpath):
+    for fy in glob.glob(op.join(vimdir, 'rcdir', "*")):
+        fcopy(fy, op.join(rcdir, op.basename(fy)), link=bool(args.link), force=args.force, test=args.test)
 
-            if 'vimrc' == fy:
-                fcopy(vpath,os.path.expanduser(files[fy]),link=bool(args.link),force=args.force,test=args.test)
-            elif '.vim' in vpath:
-                fcopy(vpath, os.path.join(ftdir,files[fy]),link=bool(args.link), force=args.force, test=args.test)
+    for fy in glob.glob(op.join(vimdir, 'ftplugin', "*")):
+        fcopy(fy, op.join(ftdir, op.basename(fy)), link=bool(args.link), force=args.force, test=args.test)
 
-            else:
-                fcopy(vpath,os.path.join(rcdir,files[fy]),link=bool(args.link),force=args.force,test=args.test)
-
-    vim_mine = os.path.expanduser('~/.vim/rcdir/vimrc.mine')
-    if not os.path.exists(vim_mine):
+    vim_mine = op.expanduser('~/.config/nvim/rcdir/vimrc.mine')
+    if not op.exists(vim_mine):
         with open(vim_mine,'a') as f:
             print >> f,'"" PC dependent vimrc'
             print >> f,'"'
             print >> f,'\n'
 
-    vim_init = os.path.expanduser('~/.vim/rcdir/vimrc.init')
-    if not os.path.exists(vim_init):
+    vim_init = op.expanduser('~/.config/nvim/rcdir/init.vim.mine')
+    if not op.exists(vim_init):
         with open(vim_init,'a') as f:
             print >> f,'"" init setting file for vim'
             print >> f,'"'
             print >> f,'\n'
+
+    src = op.join(vimdir, "vimrc")
+    dst = op.expanduser('~/.vimrc')
+    if not op.exists(dst):
+        print("link " + src + " -> " + dst)
+        os.symlink(src, dst)
+
+    src = vim_config_dir
+    dst = op.expanduser("~/.vim")
+    if not op.exists(dst):
+        print("link " + src + " -> " + dst)
+        os.symlink(src, dst)
 
