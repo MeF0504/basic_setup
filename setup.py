4f534275eba00b6a966770dfc1b2ab2c5c8d5a39
Takuro Fujino <fujino.takuro.92@gmail.com>
2019-07-09 17:17:11 +0900

    update some settings.
    * print "hoge" -> print("hoge")
    * use python modules when copy/link files

--- setup.py
+++ setup.py
@@ -12,18 +12,20 @@ import glob
 def mkdir(path):
     path = op.expanduser(path)
     if not op.exists(path):
-        print 'mkdir '+path
+        print('mkdir '+path)
         os.makedirs(path, mode=0o755)
         #os.chmod(path,0755)
 
 def fcopy(file1,file2,link=False,force=False,**kwargs):
     def fcopy_main(cmd,comment,test):
         if not test:
-            subprocess.call(cmd, shell=True)
-            print comment
+            eval(cmd)
+            print(comment)
         else:
-            print 'cmd check:: '+cmd+'\n'
+            print('cmd check:: '+cmd+'\n')
 
+    file1 = op.expanduser(file1)
+    file2 = op.expanduser(file2)
     name1 = op.basename(file1)
     name2 = op.basename(file2)
 
@@ -37,29 +39,38 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
     else:
         condition = True
 
+    if op.exists(file2):
+        exist = True
+    else:
+        exist = False
+
     if link:    #link
-        cmd = 'ln -s %s %s' % (file1, file2)
+        cmd = 'os.symlink("%s", "%s")' % (file1, file2)
         comment = 'linked '+name1
 
-        if (not op.exists(file2)) and condition:
+        if not condition:
+            print( "condition doesn't match" )
+        elif exist:
+            print('[  %s  ] is already exist, cannot link!' % name2)
+        else:
             fcopy_main(cmd,comment,test)
-        elif condition:
-            print '[  %s  ] is already exist, cannot link!' % name2
 
     else:       #copy
-        cmd = 'cp -r %s %s' % (file1, file2)
+        cmd = 'shutil.copy("%s", "%s")' % (file1, file2)
         comment = 'copy %s --> %s\n' % (name1,file2)
 
-        if force and condition:
+        if not condition:
+            print( "condition doesn't match" )
+        elif force:
             fcopy_main(cmd, comment, test)
-        elif (not op.exists(file2)) and condition:
-            fcopy_main(cmd, comment, test)
-        elif condition:
+        elif exist:
             yn = raw_input('[  %s  ] is already exist, are you realy overwite? [y,n]' % name2)
             if (yn == 'y') or (yn == 'yes'):
                 fcopy_main(cmd, comment, test)
             else:
-                print 'Do not copy '+name2+'\n'
+                print('Do not copy '+name2+'\n')
+        else:
+            fcopy_main(cmd, comment, test)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -73,13 +84,12 @@ if __name__ == "__main__":
     #print dir(args)
     #exit()
     if not op.exists(args.prefix):
-        print "install path %s does not exit" % args.prefix
+        print("install path %s does not exit" % args.prefix)
         exit()
 
     fpath = op.dirname(op.abspath(__file__))
     binpath = op.join(args.prefix,'bin')
     mkdir(binpath)
-    #print fpath
     os.chdir(fpath)
 
     if not os.environ.has_key('XDG_CONFIG_HOME'):
@@ -89,7 +99,7 @@ if __name__ == "__main__":
 
     ############### script directory ###############
     codedir = op.join(fpath,'codes')
-    print '\n@ '+codedir+'\n'
+    print('\n@ '+codedir+'\n')
     codefiles = []
     for cfy in glob.glob(op.join(codedir,'*')):
         if os.access(cfy,os.X_OK):
@@ -105,10 +115,10 @@ if __name__ == "__main__":
 
     ############### basic setup directory ###############
     setdir = op.join(fpath,'setup')
-    print '\n@ '+setdir+'\n'
+    print('\n@ '+setdir+'\n')
     files_mac = {\
                 'zshrc_file':'~/.zshrc', \
-                'matplotlibrc':'~/.matplotlib/', \
+                'matplotlibrc':'~/.matplotlib/matplotlibrc', \
                 }
 
     files_linux = {\
@@ -131,20 +141,22 @@ if __name__ == "__main__":
     zshrc_mine = op.expanduser('~/.zshrc.mine')
     if not op.exists(zshrc_mine):
         with open(zshrc_mine,'a') as f:
-            print >> f,'## PC dependent zshrc'
-            print >> f,'#'
-            print >> f,'\n'
+            f.write('## PC dependent zshrc\n')
+            f.write('#\n')
+            f.write('\n')
 
 
     ############### vim setup directory ###############
     vimdir = op.join(fpath,'vim')
-    print '\n@ '+vimdir+'\n'
+    print('\n@ '+vimdir+'\n')
 
     vim_config_dir = op.join(conf_home, 'nvim')
     rcdir = op.join(vim_config_dir, 'rcdir')
     ftdir = op.join(vim_config_dir, 'ftplugin')
+    tmdir = op.join(vim_config_dir, 'toml')
     mkdir(rcdir)
     mkdir(ftdir)
+    mkdir(tmdir)
     mkdir(op.join(vim_config_dir, "swp"))
 
     if args.download:
@@ -155,7 +167,7 @@ if __name__ == "__main__":
         #mkdir('~/.vim/bundle/')
         #subprocess.call('git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim',shell=True)
 
-        print '\nclone dein'
+        print('\nclone dein')
         mkdir(op.join(conf_home, 'nvim/dein'))
         subprocess.call('curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh && chmod u+x installer.sh && ./installer.sh %s/nvim/dein' % conf_home, shell=True)
 
@@ -182,7 +194,7 @@ if __name__ == "__main__":
         #subprocess.call('cp -ri ./current-func-info.vim/ftplugin ~/.config/nvim/',shell=True)
         #subprocess.call('cp -ri ./current-func-info.vim/plugin ~/.config/nvim/',shell=True)
 
-        print '\nremove download tmp files'
+        print('\nremove download tmp files')
         os.chdir(fpath)
         shutil.rmtree(op.join(fpath, 'tmp'))
 
@@ -193,21 +205,22 @@ if __name__ == "__main__":
     for fy in glob.glob(op.join(vimdir, 'ftplugin', "*")):
         fcopy(fy, op.join(ftdir, op.basename(fy)), link=bool(args.link), force=args.force, test=args.test)
 
-    fcopy(op.join(vimdir, 'toml'), op.join(vim_config_dir,'toml') ,  link=bool(args.link), force=args.force, test=args.test)
+    for fy in glob.glob(op.join(vimdir, 'toml', "*")):
+        fcopy(fy, op.join(tmdir, op.basename(fy)), link=bool(args.link), force=args.force, test=args.test)
 
     vim_mine = op.expanduser('~/.config/nvim/rcdir/vimrc.mine')
     if not op.exists(vim_mine):
         with open(vim_mine,'a') as f:
-            print >> f,'"" PC dependent vimrc'
-            print >> f,'"'
-            print >> f,'\n'
+            f.write('"" PC dependent vimrc\n')
+            f.write('"\n')
+            f.write('\n')
 
     vim_init = op.expanduser('~/.config/nvim/rcdir/init.vim.mine')
     if not op.exists(vim_init):
         with open(vim_init,'a') as f:
-            print >> f,'"" init setting file for vim'
-            print >> f,'"'
-            print >> f,'\n'
+            f.write('"" init setting file for vim\n')
+            f.write('"\n')
+            f.write('\n')
 
     src = op.join(vim_config_dir, "init.vim")
     dst = op.expanduser('~/.vimrc')
