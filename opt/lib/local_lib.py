e5a0a561accdaaf33af0c562f71ce4649314de4c
Takuro Fujino <fujino.takuro.92@gmail.com>
2021-09-12 15:22:51 +0900

    update chk_cmd function. enable to input full path of a command.

--- opt/lib/local_lib.py
+++ opt/lib/local_lib.py
@@ -11,9 +11,15 @@ def mkdir(path):
         #os.chmod(path,0755)
 
 def chk_cmd(cmd, verbose=False):   # check the command exists.
+    full_path = os.path.expanduser(os.path.expandvars(cmd))
+    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+        if verbose: print('find {}'.format(full_path))
+        return True
+
     if not 'PATH' in os.environ:
         if verbose: print("PATH isn't found in environment values.")
         return False
+
     for path in os.environ['PATH'].split(os.pathsep):
         cmd_path = op.join(path, cmd)
         if op.isfile(cmd_path) and os.access(cmd_path, os.X_OK):
