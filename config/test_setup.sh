e08b35c2036eb4751375b81495ed35e71c67e183
Takuro Fujino <fujino.takuro.92@gmail.com>
2022-07-04 21:03:25 +0900

    update.

--- config/test_setup.sh
+++ config/test_setup.sh
@@ -4,16 +4,17 @@ cd $(dirname $0)/..
 curdir=$(pwd)
 mkdir -p tmp_dir/bin
 
-touch tmp_dir/bin/envar
-echo 'aaa\nbbb' >> tmp_dir/bin/envar
+touch tmp_dir/bin/version.py
+echo 'aaa\nbbb' >> tmp_dir/bin/version.py
 
-ln -s $curdir/opt/bin/hoge tmp_dir/bin/pyviewer
+ln -s $curdir/opt/bin/hoge tmp_dir/bin/pytree
 
-# args="--prefix $curdir/tmp_dir"
-# args="--prefix $curdir/tmp_dir --link"
-args="--prefix $curdir/tmp_dir --test -s opt/test/setup_file_template.json"
+args="-t opt --prefix $curdir/tmp_dir"
+# args="-t opt --prefix $curdir/tmp_dir --link"
+# args="-t opt --prefix $curdir/tmp_dir --force"
+# args="-t opt --prefix $curdir/tmp_dir --test -s opt/test/setup_file_template.json"
 
-python setup.py $args
+python3 setup.py $args
 
 rm -rf tmp_dir
 
