5a35268c5ade4a78843fa3d91fab20ab36a9e589
Takuro Fujino <fujino.takuro.92@gmail.com>
2022-07-04 21:05:03 +0900

    refactored.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
 from __future__ import print_function
+
 import os
 import os.path as op
 import sys
-sys.path.append(op.join(op.dirname(__file__), 'opt/lib'))
 import argparse
 import shutil
 import subprocess
@@ -18,9 +18,8 @@ if sys.version_info.major < 3:
     import urllib as urlreq
 else:
     import urllib.request as urlreq
-#if float(sys.version[:3]) < 2.7:
-    #import commands
 
+sys.path.append(op.join(op.dirname(__file__), 'opt/lib'))
 from local_lib import mkdir, chk_cmd
 try:
     from local_lib_color import FG256, END
@@ -30,10 +29,11 @@ except ImportError:
 
 uname = platform.system()
 
-### copy func {{{
+
+# copy func {{{
 # copy file1 -> file2
-def fcopy(file1,file2,link=False,force=False,**kwargs):
-    def fcopy_main(cmd,comment,test):
+def fcopy(file1, file2, link=False, force=False, **kwargs):
+    def fcopy_main(cmd, comment, test):
         if is_color:
             fg = FG256(11)
             end = END
@@ -57,9 +57,10 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
             str2 = f.readlines()
 
         shift = '   |'
-        for line in difflib.unified_diff(str1, str2, n=1, \
-                fromfile=home_cut(file1), tofile=home_cut(file2), \
-                fromfiledate=dt1.strftime('%m %d (%Y) %H:%M:%S'), tofiledate=dt2.strftime('%m %d (%Y) %H:%M:%S')):
+        for line in difflib.unified_diff(str1, str2, n=1,
+                fromfile=home_cut(file1), tofile=home_cut(file2),
+                fromfiledate=dt1.strftime('%m %d (%Y) %H:%M:%S'),
+                tofiledate=dt2.strftime('%m %d (%Y) %H:%M:%S')):
             line = line.replace('\n', '')
             if is_color and (line[0] == '+'):
                 col = FG256(12)
@@ -112,7 +113,8 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
             if not op.exists(linkpath):
                 # broken link
                 os.unlink(file2)
-                print('{} -> {} is a broken link. unlink this.'.format(home_cut(file2), home_cut(linkpath)))
+                print('{} -> {} is a broken link. unlink this.'.format(
+                    home_cut(file2), home_cut(linkpath)))
                 exist = False
                 islink = False
         else:
@@ -121,35 +123,29 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
         exist = False
         islink = False
 
-
     shift = '  '
-    if link:    #link
+    if link:    # link
         cmd = 'os.symlink("{}", "{}")'.format(file1, file2)
         cmd = cmd.replace('\\', '\\\\')
         comment = 'linked {} --> {}'.format(name1, home_cut(file2))
 
         if not condition:
-            print(shift+"condition doesn't match" )
+            print(shift+"condition doesn't match")
         elif exist:
             if islink and filecmp.cmp(file1, file2):
                 print(shift+'[ {} ] is already linked.'.format(home_cut(file2)))
             else:
                 print(shift+'[ {} ] is already exist, cannot link!'.format(home_cut(file2)))
         else:
-            fcopy_main(cmd,comment,test)
+            fcopy_main(cmd, comment, test)
 
-    else:       #copy
+    else:       # copy
         cmd = 'shutil.copy("{}", "{}")'.format(file1, file2)
         cmd = cmd.replace('\\', '\\\\')
         comment = 'copy {} --> {}'.format(name1, home_cut(file2))
 
         if not condition:
-            print(shift+"condition doesn't match" )
-        elif force:
-            if islink:
-                print(shift+'[ {} ] is a link file, cannot copy!'.format(home_cut(file2)))
-            else:
-                fcopy_main(cmd, comment, test)
+            print(shift+"condition doesn't match")
         elif exist:
             if filecmp.cmp(file1, file2) and islink:
                 print(shift+'[ {} ] is linked.'.format(home_cut(file2)))
@@ -157,6 +153,8 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
                 print(shift+'[ {} ] is already copied.'.format(home_cut(file2)))
             elif islink:
                 print(shift+'[ {} ] is a link file, cannot copy!'.format(home_cut(file2)))
+            elif force:
+                fcopy_main(cmd, comment, test)
             else:
                 is_diff = False
                 while True:
@@ -178,7 +176,8 @@ def fcopy(file1,file2,link=False,force=False,**kwargs):
                         break
         else:
             fcopy_main(cmd, comment, test)
-#}}}
+# }}}
+
 
 def home_cut(path):
     home = op.expandvars('$HOME')
@@ -188,6 +187,7 @@ def home_cut(path):
     elif home in path:
         path = path.replace(home, '~')
     return path
+
 
 def get_files(fpath, args_type, prefix):
     if fpath is None:
@@ -213,6 +213,7 @@ def get_files(fpath, args_type, prefix):
         print("{} is not in {}. use default settings.".format(args_type, fpath))
         return None
 
+
 def show_target_files(file_dict):
     if is_color:
         fg = FG256(2)
@@ -226,12 +227,13 @@ def show_target_files(file_dict):
         print('{} => {}'.format(op.relpath(key), home_cut(file_dict[key])))
     print(fg+'=~=~=~=~=~=~=~=~=~='+end)
 
+
 def main_opt(args):
-    binpath = op.join(args.prefix,'bin')
-    libpath = op.join(args.prefix,'lib')
-    optdir = op.join(args.fpath,'opt')
-    bindir = op.join(optdir,'bin')
-    libdir = op.join(optdir, 'lib')
+    binpath = op.join(args.prefix, 'bin')
+    libpath = op.join(args.prefix, 'lib')
+    optdir = op.join(args.fpath, 'opt')
+    bindir = op.join(optdir, 'bin')
+    libdir = op.join(optdir,  'lib')
     if is_color:
         fg = FG256(10)
         end = END
@@ -245,8 +247,8 @@ def main_opt(args):
         files = {}
 
         if args.type != 'min':
-            for bfy in glob.glob(op.join(bindir,'*')):
-                if os.access(bfy,os.X_OK):
+            for bfy in glob.glob(op.join(bindir, '*')):
+                if os.access(bfy, os.X_OK):
                     fname = op.basename(bfy)
                     if (fname == 'pdf2jpg'):
                         if (uname == 'Darwin'):
@@ -254,7 +256,7 @@ def main_opt(args):
                     else:
                         files[bfy] = op.join(binpath, fname)
 
-            for lfy in glob.glob(op.join(libdir,'*')):
+            for lfy in glob.glob(op.join(libdir, '*')):
                 fname = op.basename(lfy)
                 if fname == '__pycache__':
                     continue
@@ -266,10 +268,11 @@ def main_opt(args):
         optpath = op.join(optdir, fy)
         fcopy(optpath, files[fy], link=args.link, force=args.force, test=args.test)
 
+
 def main_conf(args):
-    binpath = op.join(args.prefix,'bin')
-    libpath = op.join(args.prefix,'lib')
-    setdir = op.join(args.fpath,'config')
+    binpath = op.join(args.prefix, 'bin')
+    libpath = op.join(args.prefix, 'lib')
+    setdir = op.join(args.fpath, 'config')
     if is_color:
         fg = FG256(10)
         end = END
@@ -278,44 +281,44 @@ def main_conf(args):
         end = ''
     print('\n'+fg+'@ '+setdir+end+'\n')
 
-    files_mac = {\
-                'zshrc':'~/.zshrc', \
-                'zlogin':'~/.zlogin', \
-                'zsh/alias.zsh': '~/.zsh/alias.zsh', \
-                'zsh/complete.zsh': '~/.zsh/complete.zsh', \
-                'zsh/functions.zsh': '~/.zsh/functions.zsh', \
-                'zsh/prompt.zsh': '~/.zsh/prompt.zsh', \
-                'posixShellRC':'~/.posixShellRC',\
-                'bashrc':'~/.bashrc',\
-                'matplotlibrc':'~/.matplotlib/matplotlibrc', \
-                'gitignore_global':'~/.gitignore_global', \
-                'screenrc':'~/.screenrc', \
-                'marp/marp_MeFtheme.css':'~/.marp/marp_MeFtheme.css', \
-                'tigrc': '~/.tigrc', \
+    files_mac = {
+                'zshrc': '~/.zshrc',
+                'zlogin': '~/.zlogin',
+                'zsh/alias.zsh': '~/.zsh/alias.zsh',
+                'zsh/complete.zsh': '~/.zsh/complete.zsh',
+                'zsh/functions.zsh': '~/.zsh/functions.zsh',
+                'zsh/prompt.zsh': '~/.zsh/prompt.zsh',
+                'posixShellRC': '~/.posixShellRC',
+                'bashrc': '~/.bashrc',
+                'matplotlibrc': '~/.matplotlib/matplotlibrc',
+                'gitignore_global': '~/.gitignore_global',
+                'screenrc': '~/.screenrc',
+                'marp/marp_MeFtheme.css': '~/.marp/marp_MeFtheme.css',
+                'tigrc':  '~/.tigrc',
                 }
 
-    files_linux = {\
-                  'zshrc':'~/.zshrc', \
-                  'zlogin':'~/.zlogin', \
-                  'zsh/alias.zsh': '~/.zsh/alias.zsh', \
-                  'zsh/complete.zsh': '~/.zsh/complete.zsh', \
-                  'zsh/functions.zsh': '~/.zsh/functions.zsh', \
-                  'zsh/prompt.zsh': '~/.zsh/prompt.zsh', \
-                  'posixShellRC':'~/.posixShellRC',\
-                  'bashrc':'~/.bashrc',\
-                  'terminator_config':op.join(args.conf_home,'terminator/config'), \
-                  'terminology_base.cfg':op.join(args.conf_home, 'terminology/config/standard/base.cfg'), \
-                  'matplotlibrc':op.join(args.conf_home, 'matplotlib/matplotlibrc'), \
-                  'gitignore_global':'~/.gitignore_global', \
-                  'screenrc':'~/.screenrc', \
+    files_linux = {
+                  'zshrc': '~/.zshrc',
+                  'zlogin': '~/.zlogin',
+                  'zsh/alias.zsh': '~/.zsh/alias.zsh',
+                  'zsh/complete.zsh': '~/.zsh/complete.zsh',
+                  'zsh/functions.zsh': '~/.zsh/functions.zsh',
+                  'zsh/prompt.zsh': '~/.zsh/prompt.zsh',
+                  'posixShellRC': '~/.posixShellRC',
+                  'bashrc': '~/.bashrc',
+                  'terminator_config': op.join(args.conf_home, 'terminator/config'),
+                  'terminology_base.cfg': op.join(args.conf_home, 'terminology/config/standard/base.cfg'),
+                  'matplotlibrc': op.join(args.conf_home, 'matplotlib/matplotlibrc'),
+                  'gitignore_global': '~/.gitignore_global',
+                  'screenrc': '~/.screenrc',
                 }
 
     files_win = {}
 
-    files_min = {\
-                  'posixShellRC':'~/.posixShellRC',\
-                  'bashrc':'~/.bashrc',\
-                  'gitignore_global':'~/.gitignore_global', \
+    files_min = {
+                  'posixShellRC': '~/.posixShellRC',
+                  'bashrc': '~/.bashrc',
+                  'gitignore_global': '~/.gitignore_global',
                 }
 
     files = get_files(args.setup_file, 'config', args.prefix)
@@ -339,10 +342,10 @@ def main_conf(args):
 
     show_target_files(files)
     for fy in sorted(files.keys()):
-        spath = op.join(setdir,fy)
+        spath = op.join(setdir, fy)
         fy_dir = op.dirname(op.expanduser(files[fy]))
         if op.exists(fy_dir):
-            fcopy(spath,files[fy], link=bool(args.link), force=args.force,test=args.test)
+            fcopy(spath, files[fy], link=bool(args.link), force=args.force, test=args.test)
         else:
             print('{} does not exist, do not copy {}.'.format(fy_dir, fy))
 
@@ -357,20 +360,20 @@ def main_conf(args):
     if args.vim_prefix is not None:
         pyopt += ' --vim_prefix '+args.vim_prefix
     up_stup = \
-            "alias update_setup='cd {}".format(args.fpath.replace('\\', '\\\\').replace(' ', '\ ')) +\
-            " && git pull" +\
-            " && echo \"update? (y/[n])\"" +\
-            " && read YN" +\
-            " && [[ $YN = \"y\" ]]" +\
-            " && python3 setup.py {}".format(pyopt) +\
-            " ; cd -'"
+        "alias update_setup='cd {}".format(args.fpath.replace('\\', '\\\\').replace(' ', '\ ')) +\
+        " && git pull" +\
+        " && echo \"update? (y/[n])\"" +\
+        " && read YN" +\
+        " && [[ $YN = \"y\" ]]" +\
+        " && python3 setup.py {}".format(pyopt) +\
+        " ; cd -'"
     mine_exist = True
 
     if 'zshrc' in files:
         zshrc_mine = op.expandvars('$HOME/.zsh/zshrc.mine')
         if not op.exists(zshrc_mine):
             mkdir('~/.zsh')
-            with open(zshrc_mine,'a') as f:
+            with open(zshrc_mine, 'a') as f:
                 f.write('## PC dependent zshrc\n')
                 f.write('#\n')
                 f.write('\n')
@@ -387,7 +390,7 @@ def main_conf(args):
         bashrc_mine = op.expanduser('~/.bash/bashrc.mine')
         if not op.exists(bashrc_mine):
             mkdir('~/.bash')
-            with open(bashrc_mine,'a') as f:
+            with open(bashrc_mine, 'a') as f:
                 f.write('## PC dependent bashrc\n')
                 f.write('#\n')
                 f.write('\n')
@@ -403,8 +406,9 @@ def main_conf(args):
     if mine_exist:
         print('  update alias is\n{}'.format(up_stup))
 
+
 def main_vim(args):
-    vimdir = op.join(args.fpath,'vim')
+    vimdir = op.join(args.fpath, 'vim')
     if is_color:
         fg = FG256(10)
         end = END
@@ -433,13 +437,13 @@ def main_vim(args):
     if files is None:
         if args.type == 'min':
             files = {'vimrc': vimrc,
-                    'rcdir/vimrc_options.vim': op.join(rcpath, 'vimrc_options.vim'),
-                    'rcdir/vimrc_maps.vim': op.join(rcpath, 'vimrc_maps.vim'),
-                    'autoload/meflib.vim': op.join(alpath, 'meflib.vim'),
-                    'autoload/meflib/basic.vim': op.join(libpath, 'basic.vim'),
-                    }
+                     'rcdir/vimrc_options.vim': op.join(rcpath, 'vimrc_options.vim'),
+                     'rcdir/vimrc_maps.vim': op.join(rcpath, 'vimrc_maps.vim'),
+                     'autoload/meflib.vim': op.join(alpath, 'meflib.vim'),
+                     'autoload/meflib/basic.vim': op.join(libpath, 'basic.vim'),
+                     }
         else:
-            files = {'vimrc':vimrc}
+            files = {'vimrc': vimrc}
             for fy in glob.glob(op.join(vimdir, 'rcdir', "*")):
                 fname = op.basename(fy)
                 files[fy] = op.join(rcpath, fname)
@@ -503,14 +507,15 @@ def main_vim(args):
                 print("link " + src + " -> " + dst)
                 os.symlink(src, dst)
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prefix',help='install directory',default=op.expanduser('~/opt'))
-    parser.add_argument('--vim_prefix',help='Vim configuration directory',default=None)
-    parser.add_argument('--download',help='download some files (from git)',action='store_true')
-    parser.add_argument('--link',help="link files instead of copy",action='store_true')
-    parser.add_argument('--test',help="don't copy, just show command",action='store_true')
-    parser.add_argument('-f','--force',help="Do not prompt for confirmation before overwriting the destination path",action='store_true')
+    parser.add_argument('--prefix', help='install directory', default=op.expanduser('~/opt'))
+    parser.add_argument('--vim_prefix', help='Vim configuration directory', default=None)
+    parser.add_argument('--download', help='download some files (from git)', action='store_true')
+    parser.add_argument('--link', help="link files instead of copy", action='store_true')
+    parser.add_argument('--test', help="don't copy, just show command", action='store_true')
+    parser.add_argument('-f', '--force', help="Do not prompt for confirmation before overwriting the destination path", action='store_true')
     parser.add_argument('-t', '--type', help="set the type of copy files. If min is specified, only copy *shrc, posixShellRC, and vimrc_basic.vim.", choices='all opt config vim min'.split(), default='all')
     parser.add_argument('-s', '--setup_file', help='specify the copy files by json format setting file. please see "opt/test/setup_file_template.json" as an example.')
     args = parser.parse_args()
@@ -536,6 +541,6 @@ def main():
     if args.type in 'all vim min'.split():
         main_vim(args)
 
+
 if __name__ == "__main__":
     main()
-
