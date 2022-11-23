fc228a3ff64eb089d9486c5ed13fcbd149ea56a4
Takuro Fujino <fujino.takuro.92@gmail.com>
2022-11-23 21:20:18 +0900

    update release file list and something.

--- opt/bin/version.py
+++ opt/bin/version.py
@@ -5,20 +5,28 @@ import subprocess
 import argparse
 
 try:
-    from local_lib_color import BG, FG, END
+    from local_lib_color import BG, END
     bg = BG['c']
-except ImportError as e:
+except ImportError:
     bg = ''
     END = ''
 
+
 def linux_version():
-    release_files = ['/etc/redhat-release','/etc/lsb-release','/etc/issue']
+    release_files = ['/etc/redhat-release', '/etc/lsb-release',
+                     '/etc/lsb-release', '/etc/oracle-release',
+                     '/etc/os-release', '/etc/system-release', '/etc/issue']
+    found_file = False
     for rf in release_files:
         if os.path.exists(rf):
             with open(rf, 'r') as f:
                 for line in f:
                     print(line, end='')
+            found_file = True
             break
+    if not found_file:
+        print('any release file is not found.')
+
 
 def linux_cpu():
     cpuinfo = '/proc/cpuinfo'
@@ -28,6 +36,7 @@ def linux_cpu():
     with open(cpuinfo, 'r') as f:
         for line in f:
             print(line, end='')
+
 
 def linux_mem():
     meminfo = '/proc/meminfo'
@@ -40,10 +49,12 @@ def linux_mem():
 
 
 def mac_version():
-    subprocess.call('sw_vers',shell=True)
+    subprocess.call('sw_vers', shell=True)
+
 
 def mac_cpu():
     subprocess.call('system_profiler SPHardwareDataType', shell=True)
+
 
 def mac_mem():
     subprocess.call('system_profiler SPHardwareDataType', shell=True)
@@ -51,61 +62,44 @@ def mac_mem():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='show version etc. information of this computer.')
-    parser.add_argument('-v', '--version', help='display version.', action='store_true')
-    parser.add_argument('-c', '--cpu', help='display CPU information.', action='store_true')
-    parser.add_argument('-m', '--memory', help='display memory information.', action='store_true')
-    parser.add_argument('-a', '--all', help='display all information.', action='store_true')
+    parser.add_argument('-v', '--version', help='display version.',
+                        action='store_true')
+    parser.add_argument('-c', '--cpu', help='display CPU information.',
+                        action='store_true')
+    parser.add_argument('-m', '--memory', help='display memory information.',
+                        action='store_true')
+    parser.add_argument('-a', '--all', help='display all information.',
+                        action='store_true')
     args = parser.parse_args()
 
     uname = os.uname()[0]
-    is_display = False
-
+    show_contents = []
     if args.version:
-        is_display = True
-        if uname == 'Darwin':
-            mac_version()
-        elif uname == 'Linux':
-            linux_version()
-        else:
-            print('Sorry, this OS is not supported.')
+        show_contents += ['v']
     if args.cpu:
-        is_display = True
-        if uname == 'Darwin':
-            mac_cpu()
-        elif uname == 'Linux':
-            linux_cpu()
-        else:
-            print('Sorry, this OS is not supported.')
+        show_contents += ['c']
     if args.memory:
-        is_display = True
-        if uname == 'Darwin':
-            mac_mem()
-        elif uname == 'Linux':
-            linux_mem()
-        else:
-            print('Sorry, this OS is not supported.')
+        show_contents += ['m']
     if args.all:
-        is_display = True
-        if uname == 'Darwin':
+        show_contents = ['v', 'c', 'm']
+
+    if uname == 'Darwin':
+        if 'v' in show_contents:
             print("{}----------OS version----------{}".format(bg, END))
             mac_version()
-            print("{}----------cpu & memory information----------{}".format(bg, END))
+        if 'c' in show_contents or 'm' in show_contents:
+            print("{}----------cpu & memory information----------{}".format(
+                bg, END))
             mac_cpu()
-        elif uname == 'Linux':
+    elif uname == 'Linux':
+        if 'v' in show_contents:
             print("{}----------OS version----------{}".format(bg, END))
             linux_version()
+        if 'c' in show_contents:
             print("{}----------cpu  information----------{}".format(bg, END))
             linux_cpu()
+        if 'm' in show_contents:
             print("{}----------memory information----------{}".format(bg, END))
             linux_mem()
-        else:
-            print('Sorry, this OS is not supported.')
-
-    if not is_display:
-        if uname == 'Darwin':
-            mac_version()
-        elif uname == 'Linux':
-            linux_version()
-        else:
-            print('Sorry, this OS is not supported.')
-
+    else:
+        print('Sorry, this OS is not supported.')
