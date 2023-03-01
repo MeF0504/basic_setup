93e3542d8340dfadc145f3d8d0345eb33b7b5b89
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-03-01 14:59:30 +0900

    add grep comp.

--- config/zsh/complete.zsh
+++ config/zsh/complete.zsh
@@ -3,9 +3,32 @@
 # https://blog.freedom-man.com/zsh-completions
 # https://qiita.com/suzuki-hoge/items/0f5851bcd84176b4f46e
 
+function _grep_comp()
+{
+    _arguments \
+        '(- *)'--help'[show help]' \
+        '(- *)'{-V,--version}'[print the version]' \
+        -n'[line number]' \
+        -r'[Read all files under each directory recursively]' \
+        -i'[Ignore case distinctions]' \
+        -I'[Ignore binary files]' \
+        -l'[Print only file names.]' \
+        -m'[Stop reading a file after NUM matching lines.]' \
+        -A'[Print NUM lines after matching lines.]' \
+        -B'[Print NUM lines before matching lines.]' \
+        --exclude='[Skip files whose base name matches GLOB.]' \
+        --exclude-dir='[Exclude directories matching the pattern DIR from recursive searches.]' \
+        --include='[Search only files whose base name matches GLOB.]' \
+        '2:patterns:' \
+        '3:targets:_files'
+}
+compdef _grep_comp grep
+
+
 # pip zsh completion start
 # https://pip.pypa.io/en/stable/user_guide/#command-completion
-function _pip_completion {
+function _pip_completion
+{
   local words cword
   read -Ac words
   read -cn cword
