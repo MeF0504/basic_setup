26d364bf5ef5909c688c48850d2a4a0a91768a45
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-04-27 11:25:51 +0900

    add a completion function of copy_util.py command.

--- config/zsh/complete.zsh
+++ config/zsh/complete.zsh
@@ -37,6 +37,24 @@ function _pytree_comp()
 compdef _pytree_comp pytree
 # }}}
 
+# {{{ copy_util
+function _copy_util_comp()
+{
+    _arguments \
+        '(- *)'{-h,--help}'[show help]' \
+        --cmp'[compare each file and skip copy if files are same.]' \
+        '(-r --recursive)'--date'[copy files in the source directory to target_dir/{date}.]' \
+        '(-r --recursive -c -b -m -a --date)'{-r,--recursive}'[copy files recursively.]' \
+        '(-r --recursive)'-c'[date option: meta data update (UNIX), created (Windows)]' \
+        '(-r --recursive)'-b'[date option: created (some OS)]' \
+        '(-r --recursive)'-m'[date option: last update]' \
+        '(-r --recursive)'-a'[date option: last access]' \
+        '*:target file:_files'
+
+}
+compdef _copy_util_comp copy_util.py
+# }}}
+
 # pip zsh completion start {{{
 # https://pip.pypa.io/en/stable/user_guide/#command-completion
 function _pip_completion
