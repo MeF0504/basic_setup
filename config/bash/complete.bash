73dd212bd2df27a41d04770b4869693a6b49214c
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-04-27 12:51:14 +0900

    add a completion function of pytree command.

--- config/bash/complete.bash
+++ config/bash/complete.bash
@@ -1,5 +1,30 @@
 
-# pip bash completion start
+# pytree {{{
+_pytree_completion()
+{
+    local cur prev
+    cur=${COMP_WORDS[${COMP_CWORD}]}
+    prev=${COMP_WORDS[${COMP_CWORD}-1]}
+
+    local opts="-h -a -v -maxdepth -exclude -exfiles"
+    if [[ "${cur:0:1}" = "-" ]]; then
+        COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+    elif [[ "${prev}" = "-h" ]]; then
+        # show help
+        COMPREPLY=()
+    elif [[ "${prev}" = "-exfiles" ]]; then
+        # select file
+        COMPREPLY=( $(compgen -f -- "$cur") )
+    else
+        # select dir
+        compopt -o filenames
+        COMPREPLY=( $(compgen -d -- "$cur") )
+    fi
+}
+complete -F _pytree_completion pytree
+# }}}
+
+# pip bash completion start {{{
 # https://pip.pypa.io/en/stable/user_guide/#command-completion
 _pip_completion()
 {
@@ -8,5 +33,5 @@ _pip_completion()
                    PIP_AUTO_COMPLETE=1 $1 2>/dev/null ) )
 }
 complete -o default -F _pip_completion pip3
-# pip bash completion end
+# pip bash completion end }}}
 
