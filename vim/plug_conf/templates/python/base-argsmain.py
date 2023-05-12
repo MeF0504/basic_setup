7ee33dd298ac7aeb6b889deda76675d8c848761d
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-05-12 18:52:55 +0900

    add a template for python main function with argparse.

---
+++ vim/plug_conf/templates/python/base-argsmain.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import argparse
+
+
+def main(args):
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('{{_cursor_}}')
+	args = parer.parse_args()
+	main(args)
+
