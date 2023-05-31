1f929d414153ab8b1418fa385fef798cc883ef2b
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-05-31 15:05:49 +0900

    small update.

--- config/psfuncs/echoenvall.sh
+++ config/psfuncs/echoenvall.sh
@@ -1,5 +1,10 @@
 echoenvall()
 {
-    python -c 'import os;[print("{} :\t{}".format(k, v)) for k,v in os.environ.items()]'
+    if which python3 > /dev/null 2>&1; then
+        cmd=python3
+    else
+        cmd=python
+    fi
+    ${cmd} -c 'import os;[print("{} :\t{}".format(k, v)) for k,v in os.environ.items()]'
 }
 
