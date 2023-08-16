36f1057a253a13719f25952871ea642f6636e3b5
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-08-16 14:10:41 +0900

    show the original image at the top panel. use pymeflib.plot2.hist_np.

--- opt/bin/img_verbose.py
+++ opt/bin/img_verbose.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
+from pymeflib import plot2
+
 
 def main():
     if len(sys.argv) < 2:
@@ -12,29 +14,29 @@ def main():
         return
     img_file = sys.argv[1]
     img_data = plt.imread(img_file)
-    counts_r, bins_r = np.histogram(img_data[:, :, 0].flatten(),
-                                    bins=range(0, 255, 1))
-    bins_r = 0.5 * (bins_r[:-1] + bins_r[1:])
-    counts_g, bins_g = np.histogram(img_data[:, :, 1].flatten(),
-                                    bins=range(0, 255, 1))
-    bins_g = 0.5 * (bins_g[:-1] + bins_g[1:])
-    counts_b, bins_b = np.histogram(img_data[:, :, 2].flatten(),
-                                    bins=range(0, 255, 1))
-    bins_b = 0.5 * (bins_b[:-1] + bins_b[1:])
 
-    fig = go.Figure()
-    fig.add_trace(go.Bar(x=bins_r, y=counts_r, name='red',
-                         opacity=0.5, marker=dict(color='Red'),
-                         ))
-    fig.add_trace(go.Bar(x=bins_g, y=counts_g, name='green',
-                         opacity=0.5, marker=dict(color='Green'),
-                         ))
-    fig.add_trace(go.Bar(x=bins_b, y=counts_b, name='blue',
-                         opacity=0.5, marker=dict(color='Blue'),
-                         ))
+    fig = go.Figure().set_subplots(rows=2, cols=1)
+    # add_trace? append_trace?
+    fig.append_trace(go.Image(z=img_data), row=1, col=1)
+    plot2.hist_np(fig, img_data[:, :, 0].flatten(),
+                  xmin=0, xmax=255, bins=256,
+                  row=2, col=1,
+                  name='Red', opacity=0.5, marker=dict(color='Red'),
+                  )
+    plot2.hist_np(fig, img_data[:, :, 1].flatten(),
+                  xmin=0, xmax=255, bins=256,
+                  row=2, col=1,
+                  name='Green', opacity=0.5, marker=dict(color='Green'),
+                  )
+    plot2.hist_np(fig, img_data[:, :, 2].flatten(),
+                  xmin=0, xmax=255, bins=256,
+                  row=2, col=1,
+                  name='Blue', opacity=0.5, marker=dict(color='Blue'),
+                  )
     fig.update_layout(barmode='overlay',  # stack, group, overlay, relative
-                      xaxis=dict(title='RGB values'),
-                      yaxis=dict(title='counts'))
+                      xaxis2=dict(title='RGB values'),
+                      yaxis2=dict(title='counts'))
+
     print(f'shape: {img_data.shape}')
     fig.show()
 
