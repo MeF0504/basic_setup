c8c1d642430ac852a5a274ac30ccaf0395fc1148
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-08-30 20:07:11 +0900

    update the process.

--- opt/samples/pyviewer_fits/fits_astropy.py
+++ opt/samples/pyviewer_fits/fits_astropy.py
@@ -7,11 +7,6 @@ import numpy as np
 from . import args_chk, show_image_ndarray, help_template
 
 
-def local_color_map(data_i, max_val, min_val=0):
-    scale = 255*(data_i-min_val)/(max_val-min_val)
-    return [scale, scale, scale]
-
-
 def show_help():
     helpmsg = help_template('fits_astropy', 'show the image of fits file.' +
                             ' args.key specifies an index of HDUList, and' +
@@ -44,12 +39,13 @@ def main(fpath, args):
     data -= np.nanmin(data)
     data = np.where(data == data, data, 0)
     if hasattr(args, 'log_scale') and args.log_scale:
-        f = np.log10
-    else:
-        f = lambda x: x
-    max_val = f(np.max(data))
-    data2 = np.array([[local_color_map(f(x), max_val)
-                       if x > 0 else [0, 0, 0]
-                       for x in tmp]
-                      for tmp in data], dtype=np.uint8)
+        data = np.log10(data+1)
+    max_val = np.max(data)
+    data2 = np.zeros((data.shape[0], data.shape[1], 3))
+    data2[:, :, 0] = 255.0*data/max_val
+    data2[:, :, 1] = 255.0*data/max_val
+    data2[:, :, 2] = 255.0*data/max_val
+    data2 = data2.astype(np.uint8)
+    # 上下反転
+    data2 = data2[::-1]
     show_image_ndarray(data2, fname, args)
