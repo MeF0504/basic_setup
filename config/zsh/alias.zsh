6a5424256cbee90d3a27f977095d887e33139571
Takuro Fujino <fujino.takuro.92@gmail.com>
2023-12-21 16:26:54 +0900

    Update zshhistory.

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -8,5 +8,5 @@ alias -g L='| less -R'
 alias -g TE=' 2>&1 | tee '
 alias -g S='| sort'
 
-alias zshhistory="vi -R ~/.zsh_history"
+alias zshhistory="cat $HISTFILE | vim -c 'setlocal bt=nofile | setlocal ft=zsh' -"
 
