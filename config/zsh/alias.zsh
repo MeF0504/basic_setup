3c666771fd2a5994bbdbee1a1cdfe28df5de835b
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-01-05 11:35:12 +0900

    Update the history format of zsh.
    save the time in the history file and show the time human-friendly in
    the zshhistory command.
    (Since the conversion takes time, zshhistory command shows recent 300
    commands and zshhistory_all shows all history.)

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -8,5 +8,6 @@ alias -g L='| less -R'
 alias -g TE=' 2>&1 | tee '
 alias -g S='| sort'
 
-alias zshhistory="cat $HISTFILE | vim -c 'setlocal bt=nofile | setlocal ft=zsh' -"
+alias zshhistory="fc -l -n -t '%Y/%m/%d %H:%M:%S' -D -300 | vim -c 'setlocal bt=nofile | setlocal ft=zsh' -"
+alias zshhistory_all="fc -l -n -t '%Y/%m/%d %H:%M:%S' -D 0 | vim -c 'setlocal bt=nofile | setlocal ft=zsh' -"
 
