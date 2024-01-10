23e1ac77772ff9f862d6c88fab6d2fa15d3ac46c
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-01-10 10:09:04 +0900

    Small update.

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -9,7 +9,7 @@ alias -g TE=' 2>&1 | tee '
 alias -g S='| sort'
 
 local HIST_CMD="fc -l -n -t '%Y/%m/%d=%H:%M:%S' -D"
-local VIM_HIST_CMD="vim -c 'setlocal bt=nofile | setlocal ft=zsh | match Comment /^.*  [0-9]\+:[0-9][0-9]  /' -"
+local VIM_HIST_CMD="vim -c 'setlocal bt=nofile | setlocal ft=zsh | match Comment /^.*  [0-9]\+:[0-9][0-9]  / | normal! G' -"
 alias zshhistory="${HIST_CMD} -300 | ${VIM_HIST_CMD}"
 alias zshhistory_all="echo 'please wait...' && ${HIST_CMD} 0 | ${VIM_HIST_CMD}"
 
