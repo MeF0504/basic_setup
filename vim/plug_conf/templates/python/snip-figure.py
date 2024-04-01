1622d9799a6e4a885e85047494e4a7e91cdfcb70
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-04-01 19:04:49 +0900

    Small update.

--- vim/plug_conf/templates/python/snip-figure.py
+++ vim/plug_conf/templates/python/snip-figure.py
@@ -1,3 +1,3 @@
 fig{{_input_:fignum}} = plt.figure()
 ax{{_input_:fignum}}1 = fig{{_input_:fignum}}.add_subplot({{_input_:row}}, {{_input_:col}}, 1)
-{{_expr_:map(range(2, {{_input_:row}}*{{_input_:col}}), "printf('ax{{_input_:fignum}}%d = fig{{_input_:fignum}}.add_subplot({{_input_:row}}, {{_input_:col}}, %d)', v:val, v:val)")}}
+{{_expr_:map(range(2, {{_input_:row}}*{{_input_:col}}), "printf('ax{{_input_:fignum}}%d = fig{{_input_:fignum}}.add_subplot({{_input_:row}}, {{_input_:col}}, %d)', v:val, v:val)")}}{{_cursor_}}
