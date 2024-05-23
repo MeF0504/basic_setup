b18a582d66d5a36f8954f8206bacf5f71391424d
Takuro Fujino <fujino.takuro.92@gmail.com>
2024-05-23 15:13:51 +0900

    Fix bugs.

--- vim/autoload/meflib/git_status.py
+++ vim/autoload/meflib/git_status.py
@@ -5,16 +5,25 @@ import vim
 branch = None
 
 
+def is_err(res):
+    if len(res.stderr) > 0:
+        print(f'[git-staus error]: {res.stderr.decode()}')
+        return True
+    else:
+        return False
+
+
 def set_branch():
     global branch
     cmd = 'git branch --contains'.split()
     res = subprocess.run(cmd, capture_output=True)
-    if len(res.stdout) > 0:
-        # show error.
+    if is_err(res):
         vim.command('let g:meflib#git_status#branch = ""')
         branch = None
+        return
 
     branch = (res.stdout.decode()).split()[1]
+    branch = branch.replace('\n', '')
     vim.command(f'let g:meflib#git_status#branch = "{branch}"')
 
 
@@ -22,11 +31,12 @@ def set_update_date():
     cmd = ['git', 'log', '--date=iso',
            '--date=format:%Y/%m/%d', '--pretty=format:%ad', '-1']
     res = subprocess.run(cmd, capture_output=True)
-    if len(res.stdout) > 0:
-        # show error.
+    if is_err(res):
         vim.command('let g:meflib#git_status#date = "-/-"')
+        return
 
     date = res.stdout.decode()
+    date = date.replace('\n', '')
     if str(datetime.today().year) == date[:4]:
         date = date[5:]
         vim.command(f'let g:meflib#git_status#date = "{date}"')
@@ -38,7 +48,7 @@ def check_remote_branch_exists():
     remote = f'origin/{branch}'
     cmd = 'git branch --remotes'.split()
     res = subprocess.run(cmd, capture_output=True)
-    if len(res.stdout) > 0:
+    if is_err(res):
         return False
 
     if remote in res.stdout.decode():
@@ -48,38 +58,46 @@ def check_remote_branch_exists():
 
 
 def set_unmerged_commits():
+    # HEADからこのbranchがいくつ遅れているか
     if branch is None:
-        vim.command('let g:meflib#git_status#pre_merge = 0')
+        vim.command('let g:meflib#git_status#pre_merge = -1')
         return
 
     if not check_remote_branch_exists():
-        vim.command('let g:meflib#git_status#pre_merge = 0')
+        vim.command('let g:meflib#git_status#pre_merge = -1')
         return
 
-    cmd = ['git', 'log', '--oneline', f'HEAD..origin/f{branch}']
+    cmd = ['git', 'log', '--oneline', f'HEAD..origin/{branch}']
     res = subprocess.run(cmd, capture_output=True)
-    if len(res.stdout) > 0:
-        vim.command('let g:meflib#git_status#pre_merge = 0')
+    if is_err(res):
+        vim.command('let g:meflib#git_status#pre_merge = -1')
         return
     else:
-        num_cmts = len((res.stdout.decode()).split('\n'))
+        if len(res.stdout) == 0:
+            num_cmts = 0
+        else:
+            num_cmts = len((res.stdout.decode()).split('\n'))
         vim.command(f'let g:meflib#git_status#pre_merge = {num_cmts}')
 
 
 def set_unpushed_commits():
+    # remote にpushしていないコミット数
     if branch is None:
-        vim.command('let g:meflib#git_status#pre_push = 0')
+        vim.command('let g:meflib#git_status#pre_push = -1')
         return
 
     if not check_remote_branch_exists():
-        vim.command('let g:meflib#git_status#pre_push = 0')
+        vim.command('let g:meflib#git_status#pre_push = -1')
         return
 
-    cmd = ['git', 'rev-list', f'origin/f{branch}..f{branch}']
+    cmd = ['git', 'rev-list', f'origin/{branch}..{branch}']
     res = subprocess.run(cmd, capture_output=True)
-    if len(res.stdout) > 0:
-        vim.command('let g:meflib#git_status#pre_push = 0')
+    if is_err(res):
+        vim.command('let g:meflib#git_status#pre_push = -1')
         return
     else:
-        num_cmts = len((res.stdout.decode()).split('\n'))
+        if len(res.stdout) == 0:
+            num_cmts = 0
+        else:
+            num_cmts = len((res.stdout.decode()).split('\n'))
         vim.command(f'let g:meflib#git_status#pre_push = {num_cmts}')
