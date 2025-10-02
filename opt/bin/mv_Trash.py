0d1f8ceaadbb2a02633597b7b1959d052fa2213d
Takuro Fujino <fujino.takuro.92@gmail.com>
2025-10-02 14:45:18 +0900

    Small update.

--- opt/bin/mv_Trash.py
+++ opt/bin/mv_Trash.py
@@ -9,7 +9,10 @@ files = sys.argv[1:]
 for fy in files:
     if os.path.exists(fy):
         os.utime(fy)    # update time stamp
+        if fy.endswith('/'):
+            # If a directory ends with '/',
+            # the name is not shown in the Trash in Linux?
+            fy = fy[:-1]
         send2trash(fy)
     else:
         print("[" + fy + "] not found.")
-
